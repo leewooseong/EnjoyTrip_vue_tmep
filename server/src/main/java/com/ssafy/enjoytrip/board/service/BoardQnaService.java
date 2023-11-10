ca8b4c0917a42addc27a9125bc59c954e01cc48b
404 <schy2504@naver.com>
2023-11-11 08:08:35 +0900

    Removing List from BoardQnaService

--- server/src/main/java/com/ssafy/enjoytrip/board/service/BoardQnaService.java
+++ server/src/main/java/com/ssafy/enjoytrip/board/service/BoardQnaService.java
@@ -22,7 +22,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -141,11 +140,9 @@ public class BoardQnaService {
             }
         }
 
-        List<BoardQnaListArticleProjection> list = page.getContent();
-
         return BoardQnaListOfArticleResponseDto.builder()
                 .totalPage(page.getTotalPages())
-                .articleList(list)
+                .articleList(page.getContent())
                 .build();
     }
 
